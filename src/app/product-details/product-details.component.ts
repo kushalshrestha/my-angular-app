be4e87176ed1bf3c8dd09782f134741d5296bbfe
kushalshrestha <kushalshr@gmail.com>
2023-10-07 20:04:06 -0500

    CartService created, imported to product detail page

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 //ActivateRoute is specific to each component that the Angular Router loads. Contains information about the route and the route's parameter.
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,12 +13,22 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     const routeParams = this.route.snapshot.paramMap; //get the params from current route
     const productIdFromRoute = Number(routeParams.get('productId'));
 
-    this.product = products.find(product => product.id === productIdFromRoute);
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+
+  addProductToCart(product : Product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
